da68e8acafdf41d6156e05e405eebf7c2ad76a74
MihaiBalosu <32931832+MihaiBalosu@users.noreply.github.com>
2018-06-03 22:38:51 +0300

    Update2

--- main.c
+++ main.c
@@ -34,6 +34,27 @@ int isEmpty(struct Stack* stack)
 return (stack->top == -1);
 }
 
+void push(struct Stack *stack, int item)
+{
+	if (isFull(stack))
+		return;
+	stack -> array[++stack -> top] = item;
+}
+
+// Function to remove an item from stack. It
+// decreases top by 1
+int pop(struct Stack* stack)
+{
+	if (isEmpty(stack))
+		return INT_MIN;
+	return stack -> array[stack -> top--];
+}
+
+// Function to implement legal movement between
+// two plates
+void moveSlicesBetweenTwoPlates(struct Stack *src,
+			struct Stack *dest, char s, char d)
+
 int main()
 {
 	// Input: number of slices
