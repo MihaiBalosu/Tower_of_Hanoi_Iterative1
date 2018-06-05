884f05e5fa2cf765f70a9790b05547d1df7ea908
MihaiBalosu <32931832+MihaiBalosu@users.noreply.github.com>
2018-06-05 12:17:50 +0300

    update

---
+++ tohIterative.h
@@ -0,0 +1,8 @@
+#ifndef TOHITERATIVE_H_INCLUDED
+#define TOHITERATIVE_H_INCLUDED
+
+void tohIterative(int num_of_slices, struct Stack
+			*src, struct Stack *aux,
+			struct Stack *dest)
+
+#endif // TOHITERATIVE_H_INCLUDED
