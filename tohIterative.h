134ae9b102b3c14372e97857445c00e2853541f2
MihaiBalosu <32931832+MihaiBalosu@users.noreply.github.com>
2018-06-05 12:59:02 +0300

    Delete tohIterative.h

--- tohIterative.h
+++
@@ -1,8 +0,0 @@
-#ifndef TOHITERATIVE_H_INCLUDED
-#define TOHITERATIVE_H_INCLUDED
-
-void tohIterative(int num_of_slices, struct Stack
-			*src, struct Stack *aux,
-			struct Stack *dest)
-
-#endif // TOHITERATIVE_H_INCLUDED
